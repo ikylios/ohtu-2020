716a17a1fcf725e8b1bc4239a8779b803394cd45
ikylios <ikylioss@gmail.com>
2020-11-10 18:26:30 +0200

    3.8

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.*;
 
 public class Tester {
 
@@ -13,7 +14,57 @@ public class Tester {
         driver.get("http://localhost:4567");
         
         sleep(2);
+
+        // uuden tunnuksen luominen
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        Random r = new Random();
+        element = driver.findElement(By.name("username"));
+        String username = "uusikayttaja" + r.nextInt(100000);
+        element.sendKeys(username);
+        String password = "salainen" + r.nextInt(100000);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("signup"));
         
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+
+        // ja uloskirjautuminen
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        sleep(2);
+        element = driver.findElement(By.linkText("logout"));
+        
+        driver.quit();
+
+/*      //epäonnistunut kirjautuminen
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("vaara");
+        element = driver.findElement(By.name("login"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        
+        driver.quit();
+
+/*       //original 
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
@@ -31,6 +82,7 @@ public class Tester {
         sleep(3);
         
         driver.quit();
+*/        
     }
     
     private static void sleep(int n){
