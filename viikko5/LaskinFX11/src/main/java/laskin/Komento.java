e13ddff88298a4942181bf267b3de831b9549f11
ikylios <ikylioss@gmail.com>
2020-11-30 13:32:39 +0200

    5.4

---
+++ viikko5/LaskinFX11/src/main/java/laskin/Komento.java
@@ -0,0 +1,25 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public abstract class Komento {
+
+    protected TextField tuloskentta; 
+    protected TextField syotekentta; 
+    protected Button plus;
+    protected Button miinus;
+    protected Button nollaa;
+    protected Sovelluslogiikka sovellus;
+
+    public Komento(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+        this.plus = plus;
+        this.miinus = miinus;
+        this.nollaa = nollaa;
+        this.sovellus = sovellus;
+    }
+
+    public abstract void suorita();
+}
